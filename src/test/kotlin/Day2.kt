fecc7abfe2e03829a4dec13378e1234a2981891a
Joris Aper <joris.aper@conundra.eu>
2022-12-06 21:36:50 +0100

    part 2 + clean

--- src/test/kotlin/Day2.kt
+++ src/test/kotlin/Day2.kt
@@ -1,11 +1,12 @@
 import Day2.Result.*
+import Day2.RockPaperScissors.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.nio.charset.Charset
+import java.nio.charset.Charset.defaultCharset
 
 class Day2 {
 
-    private val input = javaClass.getResourceAsStream("day2.txt")!!.readAllBytes().toString(Charset.defaultCharset())
+    private val input = javaClass.getResourceAsStream("day2.txt")!!.readBytes().toString(defaultCharset())
 
     enum class RockPaperScissors(private val values: Array<String>, val score: Int) {
         ROCK(arrayOf("A", "X"), 1) {
@@ -30,7 +31,7 @@ class Day2 {
             }
         };
 
-        abstract fun play(rockPaperScissors: RockPaperScissors): Result
+        abstract fun play(other: RockPaperScissors): Result
 
         companion object {
             fun from(input: String) = RockPaperScissors.values().first { it.values.contains(input) }
@@ -38,33 +39,48 @@ class Day2 {
     }
 
     enum class Result(val score: Int) {
-        WINS(6), LOSES(0), DRAW(3)
-    }
+        WINS(6), LOSES(0), DRAW(3);
 
-    @Test
-    fun `rock paper scissors example`() {
-        val total = listOf(
-            "A" to "Y", // Rock -> Paper        -> win  -> 2 + 6 = 8
-            "B" to "X", // Paper -> Rock        -> loss -> 1 + 0 = 1
-            "C" to "Z", // Scissors -> Scissors -> draw -> 3 + 3 = 6 -> total: 15
-        )
-            .map { RockPaperScissors.from(it.first) to RockPaperScissors.from(it.second) }
-            .sumOf { (opponent, me) -> me.play(opponent).score + me.score }
-        assertThat(total).isEqualTo(15)
+        fun toNew(other: RockPaperScissors) = when (this) {
+            LOSES -> when (other) {
+                ROCK -> SCISSORS
+                PAPER -> ROCK
+                SCISSORS -> PAPER
+            }
+
+            DRAW -> other
+            WINS -> when (other) {
+                ROCK -> PAPER
+                PAPER -> SCISSORS
+                SCISSORS -> ROCK
+            }
+        }
+
+        companion object {
+            fun from(input: String) = when (input) {
+                "X" -> LOSES
+                "Y" -> DRAW
+                "Z" -> WINS
+                else -> throw IllegalArgumentException("wrong")
+            }
+        }
     }
 
     @Test
     fun `rock paper scissors part one`() {
-        val total = input.split("\n")
+        val result = input.split("\n")
             .map { it.split(" ") }
             .map { RockPaperScissors.from(it[0]) to RockPaperScissors.from(it[1]) }
-            .sumOf { (opponent, me) -> me.play(opponent).score + me.score }
-        assertThat(total).isEqualTo(12855)
+            .sumOf { (opponent, me) -> me.score + me.play(opponent).score }
+        assertThat(result).isEqualTo(12855)
     }
-
 
     @Test
     fun `rock paper scissors part two`() {
-        TODO()
+        val result = input.split("\n")
+            .map { it.split(" ") }
+            .map { RockPaperScissors.from(it[0]) to Result.from(it[1]).toNew(RockPaperScissors.from(it[0])) }
+            .sumOf { (opponent, me) -> me.score + me.play(opponent).score }
+        assertThat(result).isEqualTo(13726)
     }
 }
