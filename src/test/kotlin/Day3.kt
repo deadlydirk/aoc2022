c4e5fd9bfec456c8e4c75f33a895ffcb8f9c57bb
Joris Aper <joris.aper@conundra.eu>
2022-12-08 20:39:54 +0100

    day 3 part two

--- src/test/kotlin/Day3.kt
+++ src/test/kotlin/Day3.kt
@@ -1,30 +1,13 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.tuple
 import org.junit.jupiter.api.Test
-import java.nio.charset.Charset
+import java.nio.charset.Charset.defaultCharset
 import kotlin.text.CharCategory.LOWERCASE_LETTER
 import kotlin.text.CharCategory.UPPERCASE_LETTER
 
 class Day3 {
 
-    private val input = javaClass.getResourceAsStream("day3.txt")!!.readBytes().toString(Charset.defaultCharset())
-
-    class Rucksack(input: String) {
-        val compartment1: String = input.substring(0, input.length / 2)
-        val compartment2: String = input.substring(input.length / 2)
-
-        fun calculateCommonPriority() = compartment1.toCharArray().toSet()
-            .intersect(compartment2.toCharArray().toSet())
-            .first()
-            .let {
-                when (it.category) {
-                    UPPERCASE_LETTER -> it.code - 38
-                    LOWERCASE_LETTER -> it.code - 96
-                    else -> 0
-                }
-            }
-
-    }
+    private val input = javaClass.getResourceAsStream("day3.txt")!!.readBytes().toString(defaultCharset())
 
     val example = """
         vJrwpWtwJgWrhcsFMMfFFhFp
@@ -36,7 +19,7 @@ class Day3 {
     """.trimIndent()
 
     @Test
-    fun example() {
+    fun `part one - example`() {
         val result = example
             .split("\n")
             .map { Rucksack(it) }
@@ -55,9 +38,33 @@ class Day3 {
 
     @Test
     fun `part one - calculate common priorities`() {
-        val result = input
-            .split("\n")
+        val result = input.split("\n")
             .sumOf { Rucksack(it).calculateCommonPriority() }
         assertThat(result).isEqualTo(8123)
     }
+
+    @Test
+    fun `part two`() {
+        val result = input.split("\n")
+            .map { it.toCharArray().toSet() }
+            .chunked(3)
+            .sumOf { (first, second, third) -> first.intersect(second).intersect(third).first().toPriority() }
+        assertThat(result).isEqualTo(2620)
+    }
+}
+
+class Rucksack(input: String) {
+    val compartment1: String = input.substring(0, input.length / 2)
+    val compartment2: String = input.substring(input.length / 2)
+
+    fun calculateCommonPriority() = compartment1.toCharArray().toSet()
+        .intersect(compartment2.toCharArray().toSet())
+        .first()
+        .toPriority()
+}
+
+fun Char.toPriority() = when (category) {
+    UPPERCASE_LETTER -> code - 38
+    LOWERCASE_LETTER -> code - 96
+    else -> 0
 }
